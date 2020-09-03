999a5019cfb54397483f6426524bfba0a2aa94a6
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-03 09:11:20 +0100

    import add ninja and create add/delete ninja

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class  App extends Component {
   state = {
@@ -10,12 +11,30 @@ class  App extends Component {
 
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+
+  }
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    })
+    this.setState({
+      ninjas: ninjas
+    })
+
+  } 
   render() {
       return (
         <div className="App">
           <h1>My First React App</h1>
           <p>Welcome!! :)</p>
-          <Ninjas ninjas={this.state.ninjas} />
+          <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas} />
+          <AddNinja addNinja={this.addNinja} />
         </div>
   );
   }
