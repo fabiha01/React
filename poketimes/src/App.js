8badbdda3bbe25536f50ac380bca222a5725563d
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-03 12:25:36 +0100

    import navbar into app.js

--- poketimes/src/App.js
+++ poketimes/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Navbar />
 
-    </div>
-  );
+      </div>
+    );
+  }
 }
+
 
 export default App;
