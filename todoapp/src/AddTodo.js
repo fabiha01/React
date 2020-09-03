aa46ff85f10f3efb587a67400d7370a77d4d290d
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-03 11:42:38 +0100

    clear input field once todo has been addded

--- todoapp/src/AddTodo.js
+++ todoapp/src/AddTodo.js
@@ -15,13 +15,19 @@ class AddTodo extends Component {
     handleSubmit = (e) => {
         e.preventDefault();
         this.props.addTodo(this.state);
+        // will clear out the input field
+        // need to bind the value of the
+        // input field to the state
+        this.setState({
+            content: ''
+        })
     }
     render() {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                 <label>Add new todo:</label>
-                <input type="text" onChange={this.handleChange} />
+                <input type="text" onChange={this.handleChange} value={this.state.content}/>
                 </form>
             </div>
         )
