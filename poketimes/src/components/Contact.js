581e6bc06eb532ce31ed35f59fbd5e6a6b0b2308
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-03 12:25:20 +0100

    creating components

---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis non neque et dui egestas maximus. Sed dignissim, arcu a commodo lobortis, enim eros porta metus, sed ultrices tortor tellus quis ipsum. Quisque placerat urna massa, ut ullamcorper sem accumsan in. Aliquam et euismod sem. Suspendisse venenatis arcu in pharetra tincidunt. Integer quis sagittis orci. Suspendisse eu porta massa. Mauris vel nunc rutrum diam consectetur semper. Etiam tristique vestibulum pharetra. Etiam in magna ut metus efficitur luctus a ac est.</p>
+        </div>
+    )
+}
+
+export default Contact;
