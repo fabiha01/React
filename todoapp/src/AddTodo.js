f0d1b180cd2775f76546c692363379ef82993a61
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-03 11:38:41 +0100

    pass add todo as props

--- todoapp/src/AddTodo.js
+++ todoapp/src/AddTodo.js
@@ -14,7 +14,7 @@ class AddTodo extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state);
+        this.props.addTodo(this.state);
     }
     render() {
         return (
