ef84e867bd9e3398d20889a45eebf459f159eadd
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-03 16:49:53 +0100

    comment out the timeout - not needed

--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {Link, NavLink, withRouter} from 'react-router-dom';
 
 const Navbar = (props) => {
-    setTimeout(() => {
-        props.history.push('/about')
-    }, 2000)
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // }, 2000)
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
+            
                 <a className="brand-logo">Poke 'Times</a>
                 <ul className="right">
                     <li><Link to="/" >Home</Link></li>
