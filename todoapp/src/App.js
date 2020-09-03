179aed5159377ea8c47848df82af559a05fa89b5
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-03 11:26:36 +0100

    import components

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Todos from './Todos';
+import AddTodo from './AddTodo';
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play some games'}
+    ]
+  }
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos
+    })
+  }
 
-function App() {
-  return (
-    <div className="App">
-
+  render() {
+    return(
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todos</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo />
     </div>
-  );
+    )
+
+  };
 }
 
 export default App;
