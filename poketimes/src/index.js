2960ae3309a9240ca557e940464d4fb117ad158e
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-04 16:06:41 +0100

    set up redux store

--- poketimes/src/index.js
+++ poketimes/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer';
+
+// the rootReducer has been associated with the store
+// this rootReducer is what will interact with the store
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
