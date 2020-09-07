f049dd78a80faab1f5c19cc6809e00e75f0b2123
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-07 14:59:24 +0100

    import react router

--- marioplan/src/App.js
+++ marioplan/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <h1>MarioPlan</h1>
+        </div>
+      </BrowserRouter>
+    );
 
-function App() {
-  return (
-    <div className="App">
-    <h1>MarioPlan</h1>
-    </div>
-  );
+  }
+
 }
 
 export default App;
