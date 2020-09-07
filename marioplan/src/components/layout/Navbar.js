b67d69e28d3ff1a10c7c376f59f383de97bdf6b5
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-07 15:40:23 +0100

    importing the signed in and signed out components

--- marioplan/src/components/layout/Navbar.js
+++ marioplan/src/components/layout/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SignInLinks from './SignedInLinks';
+import SignOutLinks from './SignedOutLinks';
 
 const Navbar = () => {
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
             <Link to='/' className="brand-logo">MarioPlan</Link>
+            <SignInLinks />
+            <SignOutLinks />
 
             </div>
         </nav>
