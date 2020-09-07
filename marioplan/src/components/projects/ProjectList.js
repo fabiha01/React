066603e4606e8dd978396a8c3faf45118c079020
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-07 16:10:05 +0100

    create the project list component

---
+++ marioplan/src/components/projects/ProjectList.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const ProjectList = () => {
+    return (
+        <div className="project-list section">
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Fabiha</p>
+                    <p className="grey-text">7th September 4pm</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Fabiha</p>
+                    <p className="grey-text">7th September 4pm</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by Fabiha</p>
+                    <p className="grey-text">7th September 4pm</p>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default ProjectList;
