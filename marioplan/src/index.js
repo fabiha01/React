257caa0d7109151c79f812cef98133f1bb24d128
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-08 15:41:40 +0100

    import firebase/firestore and pass in config

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import {reduxFireStore, getFireStore} from 'redux-firestore';
+import {reactReduxFirebase, getFireBase} from 'react-redux-firebase';
+import fbConfig from './config/fbConfig';
 
 // creating store
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer,
+    compose(
+      applyMiddleware(thunk.withExtraArgument({getFireBase, getFireStore})),
+      reduxFireStore(fbConfig),
+      reactReduxFirebase(fbConfig)
+    )
+  );
 
 ReactDOM.render(
   <React.StrictMode>
