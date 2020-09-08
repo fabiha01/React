90ba5576eb5a75d1b0b6cd649e7da97b420d9cff
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-08 11:41:16 +0100

    adding data to state

--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
 const initState = {
+    projects: [
+        {id: '1', title: 'Help me find peach', content: 'blah blah blah'},
+        {id: '2', title: 'Collect all stars', content: 'blah2 blah blah'},
+        {id: '3', title: 'egg hunt with Yoshi', content: 'blah3 blah blah'}
+
+    ]
 
 }
 
