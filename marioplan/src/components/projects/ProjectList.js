bc24d942113fb26e5222b3b9372e09a2ae77c1d0
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-08 11:41:03 +0100

    using the prop to pass in data to project summary

--- marioplan/src/components/projects/ProjectList.js
+++ marioplan/src/components/projects/ProjectList.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ProjectSummary from './ProjectSummary';
 
-const ProjectList = () => {
+// we are taking the projects as a prop
+const ProjectList = ({projects}) => {
     return (
         <div className="project-list section">
-            <ProjectSummary />
-            <ProjectSummary />
-            <ProjectSummary />
-            <ProjectSummary />
+            {projects && projects.map(project => {
+                // ^ we cycle through the projects
+                return (
+                    // we return JSX
+                    // for each project summmary we're passing down a project
+                    // and setting the prokect id as the key
+                    <ProjectSummary project={project} key={project.id}></ProjectSummary>
+                )
+            })}
         </div>
 
     )
