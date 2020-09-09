c3dbf756356a1acac1509ed63bb9ac4af7ffa403
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-09 10:02:59 +0100

    update firebase config

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -7,23 +7,39 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import {reduxFireStore, getFireStore} from 'redux-firestore';
-import {reactReduxFirebase, getFireBase} from 'react-redux-firebase';
-import fbConfig from './config/fbConfig';
+import firebase from './config/fbConfig';
+import {createFirestoreInstance, reduxFirestore, getFirestore} from 'redux-firestore';
+import { ReactReduxFirebaseProvider, getFirebase} from 'react-redux-firebase';
+import 'firebase/firestore';
+
+const rrfConfig = {
+  userProfile: 'projects',
+  useFirestoreForProfile: true
+}
+
 
 // creating store
 const store = createStore(rootReducer,
     compose(
-      applyMiddleware(thunk.withExtraArgument({getFireBase, getFireStore})),
-      reduxFireStore(fbConfig),
-      reactReduxFirebase(fbConfig)
+      applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+      reduxFirestore(firebase),
     )
   );
+  
+const rffProps = {
+  firebase,
+  useFirestoreForProfile: true,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 ReactDOM.render(
   <React.StrictMode>
   <Provider store={store}>
-    <App />
+    <ReactReduxFirebaseProvider {...rffProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
   </Provider>
   </React.StrictMode>,
   document.getElementById('root')
