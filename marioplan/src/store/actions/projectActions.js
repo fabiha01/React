9f7a186e40d1982580df2b6a235c19f871b4367d
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-09 10:03:29 +0100

    update reducers to use firebase firestore

--- marioplan/src/store/actions/projectActions.js
+++ marioplan/src/store/actions/projectActions.js
@@ -1,7 +1,25 @@
 export const createProject = (project) => {
-    return (dispatch, getState, {getFireBase, getFireStore}) => {
+    return (dispatch, getState, {getFireBase, getFirestore}) => {
         // make async call to database
-        dispatch({type: 'CREATE_PROJECT', project});
+
+        // this gives us a reference to our firestore database
+        const fireStore = getFirestore();
+        fireStore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Net',
+            authorLastName: 'Ninja',
+            authorId: 1234,
+            createdAt: new Date()
+
+        }).then(() => {
+
+            // the action is the messsage
+            // the reducer listens for the action and then updates the action
+            dispatch({type: 'CREATE_PROJECT', project});
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err})
+        })
+        
 
     }
 };
