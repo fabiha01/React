385b0c5a17a8e4a1f4f08a326efffda70c80aecd
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2020-09-10 09:59:18 +0100

    learning about named and default exports

--- Redux/teacher.js
+++ Redux/teacher.js
@@ -1,7 +1,12 @@
 import {Person} from './person';
 
+export  function promote() {
+
+}
+
 // this is an example of inheritance
-export class Teacher extends Person{
+// the teacher export is the main function 
+export default class Teacher extends Person{
     constructor(name, degree) {
         super(name); // this will initialise the name property
         this.degree = degree;
