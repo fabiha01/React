4b297fd28b211aeeb8f758e87bc3dbf3ed8363b0
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-08-03 18:13:47 +0100

    fix: testing dependencies in useEffect

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -9,6 +9,8 @@ const Home = () => {
     { title: 'Developer top tips', body: 'lorem ipsum again...', author: 'mario', id: 3 }
   ]);
 
+  const [name, setName] = useState('mario');
+
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
@@ -16,12 +18,14 @@ const Home = () => {
 
   useEffect(() => {
     console.log('use effect ran');
-    console.log(blogs);
-  });
+    console.log(name);
+  }, [name]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <button onClick={() => setName('luigi')}>Change name</button>
+      <p>{name}</p>
     </div>
   );
 }
