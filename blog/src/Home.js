dbbe600d5ce73195614bcdd020e13f9ca0961800
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-08-05 16:39:37 +0100

    fix: handle errors

--- blog/src/Home.js
+++ blog/src/Home.js
@@ -5,23 +5,33 @@ const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-        setIsLoading(false);
-      });
+        .then(res => {
+          if ( !res.ok ) {
+            throw Error('Could not fetch the data');
+          }
+          console.log(res);
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsLoading(false);
+          setError(null);
+        })
+        .catch(err => {
+          setIsLoading(false);
+          setError(err.message);
+        });
     }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div> {error} </div>}
       {isLoading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
