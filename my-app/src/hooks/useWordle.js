7a966209aaab3efcfb962f8de5c5c6d0f2ff0c81
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-08-22 12:10:45 +0100

    chore: formatting

--- my-app/src/hooks/useWordle.js
+++ my-app/src/hooks/useWordle.js
@@ -1,32 +1,34 @@
-import { useState } from "react";n 
+import { useState } from "react";
 
 const useWordle = (solution) => {
-  const [turn, setTurn] = useState(0);
-  const [currentGuess, setCurrentGuess] = useState('');
-  const [guesses, setGuesses] = useState([]); // each guess is an array
-  const [history, setHistory] = useState([]); // each guess is a string
-  const [isCorrect, setIsCorrect] = useState(false);  
+  const [turn, setTurn] = useState(0) 
+  const [currentGuess, setCurrentGuess] = useState('')
+  const [guesses, setGuesses] = useState([]) // each guess is an array
+  const [history, setHistory] = useState([]) // each guess is a string
+  const [isCorrect, setIsCorrect] = useState(false)
   
-  // format a guess into an array of letter objects
+  // format a guess into an array of letter objects 
   // e.g. [{key: 'a', color: 'yellow'}]
   const formatGuess = () => {
-
+    
   }
 
   // add a new guess to the guesses state
-  // update isCorrect state if the guess is correct
-  // add 1 to the turn state
+  // update the isCorrect state if the guess is correct
+  // add one to the turn state
   const addNewGuess = () => {
 
   }
 
+
   // handle key up event & track current guess
   // if user presses enter, add new guess
-  const handleKeyup = () => {
-
+  const handleKeyup = ({ key }) => {
+    console.log(key);
+    
   }
 
-  return {turn, currentGuess, guesses, history, isCorrect, handleKeyup};
+  return {turn, currentGuess, guesses, isCorrect, handleKeyup};
 
 }
 
