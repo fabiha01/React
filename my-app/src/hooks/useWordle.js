8c01b71c70bfd15d8b9b4c49ddace332f606a8f2
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-08-22 11:39:23 +0100

    fix: create useWordle skeleton

--- my-app/src/hooks/useWordle.js
+++ my-app/src/hooks/useWordle.js
@@ -1,6 +1,32 @@
+import { useState } from "react";n 
+
 const useWordle = (solution) => {
+  const [turn, setTurn] = useState(0);
+  const [currentGuess, setCurrentGuess] = useState('');
+  const [guesses, setGuesses] = useState([]); // each guess is an array
+  const [history, setHistory] = useState([]); // each guess is a string
+  const [isCorrect, setIsCorrect] = useState(false);  
   
-  
+  // format a guess into an array of letter objects
+  // e.g. [{key: 'a', color: 'yellow'}]
+  const formatGuess = () => {
+
+  }
+
+  // add a new guess to the guesses state
+  // update isCorrect state if the guess is correct
+  // add 1 to the turn state
+  const addNewGuess = () => {
+
+  }
+
+  // handle key up event & track current guess
+  // if user presses enter, add new guess
+  const handleKeyup = () => {
+
+  }
+
+  return {turn, currentGuess, guesses, history, isCorrect, handleKeyup};
 
 }
 
