22e92e0d7e3b9b47591e0d147c4706c59200e1cc
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-08-25 14:17:14 +0100

    fix: initial creation of the useWordle hook

--- my-app/src/hooks/useWordle.js
+++ my-app/src/hooks/useWordle.js
@@ -10,7 +10,26 @@ const useWordle = (solution) => {
   // format a guess into an array of letter objects 
   // e.g. [{key: 'a', color: 'yellow'}]
   const formatGuess = () => {
-    
+    let solutionArray = [...solution];   
+    let formattedGuess = [...currentGuess].map((l) => {
+      return { key: l, color: 'grey' }
+    });
+
+    // find any green letters
+    formattedGuess.forEach((l, i) => {
+      if (solutionArray[i] === l.key ) {
+        formattedGuess[i].color = 'green';
+        solutionArray[i] = null;
+      }
+    });
+
+    // find any yellow letters
+    formattedGuess.forEach((l, i) => {
+      if (solutionArray.includes(l.key)) {
+        formattedGuess[i].color = 'yellow';
+      }
+    })
+
   }
 
   // add a new guess to the guesses state
@@ -24,7 +43,43 @@ const useWordle = (solution) => {
   // handle key up event & track current guess
   // if user presses enter, add new guess
   const handleKeyup = ({ key }) => {
-    console.log(key);
+
+    if ( key === 'Enter' ) {
+      // only add guess if turn is less than 5
+      if (turn > 5) {
+        console.log('You have used all of your guesses');
+        return;
+      }
+
+      // do not allow duplicate words
+      if (history.includes(currentGuess)) {
+        console.log('You have already tried that word');
+        return;
+      }
+
+      // check word is 5 chars long
+      if (currentGuess.length !== 5) {
+        console.log('Word must be 5 chars long');
+        return;
+      }
+
+      formatGuess();
+    }
+    
+    if (key === 'Backspace') {
+      setCurrentGuess((prev) => {
+        return prev.slice(0, -1);
+      })
+      return;
+    }
+
+    if (/^[A-Za-z]$/.test(key)) {
+      if ( currentGuess.length < 5 ) {
+        setCurrentGuess((prev) => {
+          return prev + key;
+        })
+      }
+    }
     
   }
 
