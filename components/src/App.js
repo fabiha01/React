8e2191553879af3feb937a713db6e7d15f3eb7c1
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-09-26 20:50:46 +0100

    fix: import comment detail component

--- components/src/App.js
+++ components/src/App.js
@@ -1,52 +1,9 @@
-import { faker } from "@faker-js/faker";
+import CommentDetail from "./CommentDetail";
 
 const App = ()  => {
   return (
     <div className="ui comments">
-      <div class="comment">
-        <a class="avatar">
-          <img src={faker.image.avatar()} alt="avatar" />
-        </a>
-        <div class="content">
-          <a class="author">Matt</a>
-          <div class="metadata">
-            <span class="date">Today at 5:42PM</span>
-          </div>
-          <div class="text">
-            How artistic!
-          </div>
-        </div>
-      </div>
-      <div class="comment">
-        <a class="avatar">
-          <img src={faker.image.avatar()} alt="avatar" />
-        </a>
-        <div class="content">
-          <a class="author">Elliot Fu</a>
-          <div class="metadata">
-            <span class="date">Yesterday at 12:30AM</span>
-          </div>
-          <div class="text">
-            <p>This has been very useful for my research. Thanks as well!</p>
-          </div>
-        </div>
-        <div class="comments">
-          <div class="comment">
-            <a class="avatar">
-            <img src={faker.image.avatar()} alt="avatar" />
-            </a>
-            <div class="content">
-              <a class="author">Jenny Hess</a>
-              <div class="metadata">
-                <span class="date">Just now</span>
-              </div>
-              <div class="text">
-                Elliot you are always so right :)
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <CommentDetail />      
     </div>
   );
 }
