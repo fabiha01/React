d874aa110ce11c65856bddb0c3044b0fc63ce645
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-09-27 14:56:45 +0100

    fix: import faker and add props

--- components/src/App.js
+++ components/src/App.js
@@ -1,11 +1,12 @@
 import CommentDetail from "./CommentDetail";
+import { faker } from "@faker-js/faker";
 
 const App = ()  => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />  
-      <CommentDetail author="alex" />  
-      <CommentDetail author="jane" />  
+      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" image={faker.image.avatar()} blogText="Whoa!" />  
+      <CommentDetail author="alex" timeAgo="Today at 2:00AM" image={faker.image.avatar()} blogText="More New Recipes" />  
+      <CommentDetail author="jane" timeAgo="Yesterday at 5:00PM" image={faker.image.avatar()} blogText="My new blog" />  
     </div>
   );
 }
