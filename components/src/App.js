81d071ff85ce6558c1ee7505db0c306646ea2143
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-09-27 20:15:11 +0100

    fix: import and use approval card component

--- components/src/App.js
+++ components/src/App.js
@@ -1,9 +1,11 @@
 import CommentDetail from "./CommentDetail";
 import { faker } from "@faker-js/faker";
+import ApprovalCard from "./ApprovalCard";
 
 const App = ()  => {
   return (
     <div className="ui container comments">
+      <ApprovalCard />
       <CommentDetail author="Sam" timeAgo="Today at 4:45PM" image={faker.image.avatar()} blogText="Whoa!" />  
       <CommentDetail author="alex" timeAgo="Today at 2:00AM" image={faker.image.avatar()} blogText="More New Recipes" />  
       <CommentDetail author="jane" timeAgo="Yesterday at 5:00PM" image={faker.image.avatar()} blogText="My new blog" />  
