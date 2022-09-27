64fbd0602d14e63d45cc289fef9354790faf1418
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-09-27 14:57:02 +0100

    fix: add props

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -1,19 +1,17 @@
-import { faker } from "@faker-js/faker";
-
 const CommentDetail = props => {
-  const { author } = props;
+  const { author, timeAgo, image, blogText } = props;
   return ( 
     <div className="comment">
       <a className="avatar">
-        <img src={faker.image.avatar()} alt="avatar" />
+        <img src={ image } alt="avatar" />
       </a>
       <div className="content">
         <a className="author">{ author }</a>
         <div className="metadata">
-          <span className="date">Today at 5:42PM</span>
+          <span className="date">{ timeAgo }</span>
         </div>
         <div className="text">
-          How artistic!
+         { blogText }
         </div>
       </div>
     </div>
