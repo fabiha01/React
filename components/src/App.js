7fe8b32741787d1d06775447fa34d34c3e22e5ab
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-09-27 20:22:17 +0100

    fix: nest CommentDetail in ApprovalCard component

--- components/src/App.js
+++ components/src/App.js
@@ -5,10 +5,18 @@ import ApprovalCard from "./ApprovalCard";
 const App = ()  => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" image={faker.image.avatar()} blogText="Whoa!" />  
-      <CommentDetail author="alex" timeAgo="Today at 2:00AM" image={faker.image.avatar()} blogText="More New Recipes" />  
+      <ApprovalCard>
+        <CommentDetail author="Sam" timeAgo="Today at 4:45PM" image={faker.image.avatar()} blogText="Whoa!" />  
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail author="alex" timeAgo="Today at 2:00AM" image={faker.image.avatar()} blogText="More New Recipes" />  
+      </ApprovalCard>
+     
+     <ApprovalCard>
       <CommentDetail author="jane" timeAgo="Yesterday at 5:00PM" image={faker.image.avatar()} blogText="My new blog" />  
+     </ApprovalCard>
+      
     </div>
   );
 }
