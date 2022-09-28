cf05e4ffe346d7b337d1366181c94fd393e380c1
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-09-28 13:37:36 +0100

    fix: refactor to class based component

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -1,15 +1,15 @@
+import React from 'react';
 import './App.css';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  );
-
-
-  return (
-    <div>Hi There!</div>
-  );
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err)
+    );
+    
+    return <div>Latitude</div>
+  }
 }
 
 export default App;
