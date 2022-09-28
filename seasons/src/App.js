b9cc0256d63e51f0381653ce2ec250c3db02a76a
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-09-28 14:51:05 +0100

    fix: use constructor and this.state within class component

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null
+    };
+
     window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
       err => console.log(err)
     );
+  }
+
+  render() {
     
-    return <div>Latitude</div>
+    return <div>Latitude {this.state.lat}</div>
   }
 }
 
