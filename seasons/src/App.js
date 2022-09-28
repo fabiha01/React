d25342cdefaf71c281d2cffbd7a2b728271f385b
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-09-28 20:04:53 +0100

    fix: handle error

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -6,20 +6,29 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      lat: null
+      lat: null,
+      errorMessage: ''
     };
 
     window.navigator.geolocation.getCurrentPosition(
       position => {
-        this.setState({ lat: position.coords.latitude });
+        this.setState( { lat: position.coords.latitude } );
       },
-      err => console.log(err)
+      err => {
+        this.setState( { errorMessage: err.message } );
+      }
     );
   }
 
   render() {
     
-    return <div>Latitude {this.state.lat}</div>
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
   }
 }
 
