65de69b138341385b4f8d410746b7390ac1df0a6
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-09-28 10:53:55 +0100

    fix: adding geolocation test code

--- seasons/src/App.js
+++ seasons/src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position),
+    err => console.log(err)
+  );
+
+
   return (
     <div>Hi There!</div>
   );
