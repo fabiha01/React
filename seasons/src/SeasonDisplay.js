a7e18e07a37e0ac53d34045f0ddc0cba0b60782d
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-09-29 14:28:09 +0100

    fix: add a config for season to clean up code

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,3 +1,14 @@
+const seasonConfig = {
+  summer: {
+    text: 'Let\'s hit the beach!',
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr, it\'s chilly!',
+    iconName: 'snowflake'
+  }
+};
+
 const getSeason = ( lat, month ) => {
   if ( month > 2 && month < 9 ) {
     return lat > 0 ? 'summer' : 'winter';
@@ -8,14 +19,13 @@ const getSeason = ( lat, month ) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason( props.lat, new Date().getMonth() );
-  const text  = season === 'winter' ? 'Burr, it\'s chilly!' : 'Lets hit the beach!';
-  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  const { text, iconName } = seasonConfig[season];
 
   return ( 
     <div>
-      <i className={`icon ${icon}`}></i>
+      <i className={`icon ${iconName}`}></i>
       <h1>{ text }</h1>
-      <i className={`icon ${icon}`}></i>
+      <i className={`icon ${iconName}`}></i>
     </div>
    );
 }
