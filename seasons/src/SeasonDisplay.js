7810a6df2f420ed430dffefd4cde80cc38cbaac4
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-09-29 14:14:54 +0100

    fix: add icons

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -9,10 +9,13 @@ const getSeason = ( lat, month ) => {
 const SeasonDisplay = (props) => {
   const season = getSeason( props.lat, new Date().getMonth() );
   const text  = season === 'winter' ? 'Burr, it\'s chilly!' : 'Lets hit the beach!';
+  const icon = season === 'winter' ? 'snowflake' : 'sun';
 
   return ( 
     <div>
+      <i className={`icon ${icon}`}></i>
       <h1>{ text }</h1>
+      <i className={`icon ${icon}`}></i>
     </div>
    );
 }
