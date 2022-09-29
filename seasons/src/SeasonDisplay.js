083d992bcbd97f6befe02e79564890bcc2080981
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-09-29 14:09:42 +0100

    fix: update which season to display

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -8,11 +8,12 @@ const getSeason = ( lat, month ) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason( props.lat, new Date().getMonth() );
+  const text  = season === 'winter' ? 'Burr, it\'s chilly!' : 'Lets hit the beach!';
 
-  console.log(season);
-  
   return ( 
-    <div>Season Display: </div>
+    <div>
+      <h1>{ text }</h1>
+    </div>
    );
 }
  
