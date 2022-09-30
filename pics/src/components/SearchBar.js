8327699985c2906edadcd4a9f2847b567102b8e1
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-09-30 16:01:56 +0100

    chore: formatting

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -10,7 +10,7 @@ class SearchBar extends React.Component {
           <div className="field">
             <label>Image Search</label>
             <input type="text" 
-              value={this.state.term} 
+              value={this.state.term}
               onChange={e => this.setState( { term: e.target.value } ) }
             />
           </div>
