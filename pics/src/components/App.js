1c4b0386ac36a503d774742ffaee5e364636e2c6
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-09-30 09:39:07 +0100

    chore: formatting

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,11 +1,9 @@
-import '../App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          App
         </p>
       </header>
     </div>
