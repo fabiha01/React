1b15c6fc2d94ecca3ffb62306240fcc345f86017
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-09-30 09:43:49 +0100

    fix: create search bar component

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+  render() {
+    return ( 
+      <div>
+        <form>
+          <input type="text" name="" id="" />
+        </form>
+      </div>
+    );
+  }
+
+}
+ 
+export default SearchBar;
