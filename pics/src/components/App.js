e68a8f283ec4d7e87f5f88c7a1841a2703d7a85d
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-10-04 16:24:16 +0100

    fix: clean up axios code

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   state = { images: [] };
 
    onSearchSubmit = async term => {
-   const response = await axios.get('https://api.unsplash.com/search/photos', {
-      params: { query: term }, 
-      headers: {
-        Authorization: 'Client-ID 0jSSpL1RxeagoGG6XTf-OyZVMlQC2E-kdUtvEyR02xA'
-      }
+   const response = await unsplash.get('search/photos', {
+      params: { query: term }
     });
     
     this.setState( { images: response.data.results } );
