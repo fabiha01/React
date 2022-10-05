161cb841a5bd1c08af71016950a3dc1b5dff0c64
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-10-05 16:42:47 +0100

    fix: use image id as the key

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,7 +1,7 @@
 const ImageList = (props) => {
 
-  const images = props.images.map((image, i) => {
-    return <img src={image.urls.regular} alt={image.alt_description} key={i}/>
+  const images = props.images.map(image => {
+    return <img src={image.urls.regular} alt={image.alt_description} key={image.id}/>
   })
 
   return ( 
