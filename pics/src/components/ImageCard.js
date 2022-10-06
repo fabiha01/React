07c7cc026ea94c6438601faf22cd7c9dac92a2ec
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-10-06 16:29:40 +0100

    fix: using refs to get image height

--- pics/src/components/ImageCard.js
+++ pics/src/components/ImageCard.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
+
+  // after it gets rendered, componentDidMount gets called
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight);
+  }
+
   render() {
     const { alt_description, urls } = this.props.image;
 
     return ( 
       <div>
-        <img 
+        <img
+          ref={this.imageRef}
           src={urls.regular} 
           alt={alt_description} 
         />
