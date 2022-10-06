c702b59111899c51fc2d028b8d904aae585d3560
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-10-06 16:07:11 +0100

    feat: create ImageCard component

---
+++ pics/src/components/ImageCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+  render() {
+    const { alt_description, urls } = this.props.image;
+
+    return ( 
+      <div>
+        <img 
+          src={urls.regular} 
+          alt={alt_description} 
+        />
+      </div>
+     );
+  }
+}
+ 
+export default ImageCard;
