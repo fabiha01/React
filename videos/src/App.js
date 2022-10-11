32fa7dc22727903ef4af77dd7698ab8bff45f1b6
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-10-11 10:06:24 +0100

    fix: add semantic classes to app

--- videos/src/App.js
+++ videos/src/App.js
@@ -4,7 +4,9 @@ import SearchBar from "./components/SearchBar";
 class App extends React.Component {
   render() {
     return (
-      <div><SearchBar /></div>
+      <div className="ui container">
+        <SearchBar />
+      </div>
     )
   }
 }
