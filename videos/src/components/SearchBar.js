584c55e333a0011e09cdaa7599e2a46d62e5e3e0
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-10-11 10:10:23 +0100

    fix: adding onSubmit function

--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,16 +1,23 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  state = { term: 'test' };
+  state = { term: '' };
 
-  onInputChange = () => {
-
+  onInputChange = e => {
+    this.setState({ term: e.target.value });
   };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+
+    // TODO: make sure we call
+    // callback from parent component
+  }
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Video Search</label>
             <input 
