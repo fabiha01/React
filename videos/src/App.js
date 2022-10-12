aa94a6cc1b4f9ce9b8582dec8a075dc99d5bcd4b
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-10-12 08:40:00 +0100

    fix: import VideoList component

--- videos/src/App.js
+++ videos/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./components/SearchBar";
 import youtube from "./apis/youtube";
+import VideoList from "./components/VideoList";
 
 class App extends React.Component {
 
@@ -21,7 +22,7 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        I have {this.state.videos.length} videos.
+        <VideoList videos={this.state.videos} />
       </div>
     )
   }
