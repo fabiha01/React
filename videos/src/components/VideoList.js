83d3235e2ce1ebf49fdad5e5fc3bd04855b6453f
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-10-12 08:57:26 +0100

    fix: import VideoList component

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,7 +1,12 @@
+import VideoItem from "./VideoItem";
+
 const VideoList = ( { videos } ) => {
+  const renderedList = videos.map( video => {
+    return <VideoItem />;
+  });
 
   return ( 
-    <div>{ videos.length }</div>
+    <div>{ renderedList }</div>
    );
 }
  
