f69fb17a4ab092009d772ea13e530be2f891a6eb
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-10-12 09:02:12 +0100

    fix: add image and title of video

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,6 +1,9 @@
-const VideoItem = () => {
+const VideoItem = ( { video } ) => {
   return ( 
-    <div>VideoItem</div>
+    <div>
+      <img src={ video.snippet.thumbnails.medium.url } />
+      { video.snippet.title }
+    </div>
    );
 }
  
