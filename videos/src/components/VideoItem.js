adfee6cf12b4e977337d6ef12ad37401988553d4
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-10-13 16:21:30 +0100

    chore: formatting

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -1,16 +1,16 @@
 import './VideoItem.css';
 
-const VideoItem = ( { video } ) => {
-  return ( 
-    <div className="video-item item">
-      <img className="ui image" src={ video.snippet.thumbnails.medium.url } alt={ video.snippet.description } />
+const VideoItem = ({ video, onVideoSelect }) => {
+  return (
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
+      <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.snippet.description} />
       <div className="content">
         <div className="header">
-          { video.snippet.title }
+          {video.snippet.title}
         </div>
       </div>
     </div>
-   );
+  );
 }
- 
+
 export default VideoItem;
