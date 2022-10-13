838832f31489a9dff51fb72f152a75985683f539
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-10-13 16:21:17 +0100

    fix: add state for selected video

--- videos/src/App.js
+++ videos/src/App.js
@@ -5,7 +5,7 @@ import VideoList from "./components/VideoList";
 
 class App extends React.Component {
 
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
@@ -17,12 +17,16 @@ class App extends React.Component {
     this.setState({ videos: response.data.items });
   }
 
+  onVideoSelect = video => {
+    console.log(`from the app! ${video}`);
+  };
+
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
       </div>
     )
   }
