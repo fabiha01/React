cb9e25c292594eb4a04da30eaa332dde0c74bd04
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-10-18 08:57:28 +0100

    fix: import Accordion component and render items

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,8 +1,25 @@
-import './App.css';
+import Accordion from "./components/Accordion";
+
+const items = [
+  {
+    title: 'What is React',
+    content: 'React is a front end JavaScript framework'
+  },
+  {
+    title: 'Why use React',
+    content: 'React is a favourite JS library amonst engineers'
+  },
+  {
+    title: 'How to use React',
+    content: 'You use React by creating components'
+  }
+]
 
 function App() {
   return (
-    <h1>Widgets App</h1>
+   <div>
+     <Accordion items={items} />
+   </div>
   );
 }
 
