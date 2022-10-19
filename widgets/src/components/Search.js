6a4d55a539a5617645331ae1b7c5cf8e3d78c41e
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-10-19 08:56:34 +0100

    fix: useState for search term

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,6 +1,22 @@
+import { useState } from "react";
+
 const Search = () => {
+  const [term, setTerm] = useState(''); 
+
   return ( 
-    <h1>Search</h1>
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input 
+            value={term} 
+            onChange={(e) => setTerm(e.target.value)} 
+            type="text" 
+            className="input" 
+          />
+        </div>
+      </div>
+    </div>
    );
 }
  
