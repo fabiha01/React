743bc33174af4b8cd12fdbdf97354dca49866101
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-10-19 09:59:34 +0100

    fix: use axios to do a get request to wikipedia

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -5,7 +5,21 @@ const Search = () => {
   const [term, setTerm] = useState('');
 
   useEffect(() => {
-  });
+    const search = async () => {
+      await axios.get(' https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      });
+    };
+
+    search();
+
+  }, [term]);
 
   return ( 
     <div>
