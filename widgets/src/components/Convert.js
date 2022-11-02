4f454b487ddb03b8ed44bd775c38ebaaa3b102a8
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-11-02 10:55:26 +0000

    feat: create convert component

---
+++ widgets/src/components/Convert.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+
+const Convert = ({language, text}) => {
+  useEffect(() => {
+    console.log('New language or text');
+  }, [language, text]);
+
+  return ( 
+    <div></div>
+   );
+}
+ 
+export default Convert;
