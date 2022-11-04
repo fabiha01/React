2bd644925037ca6474f64e3af896d2b6b542d13f
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-11-04 17:02:56 +0000

    fix: import custom useVideos hooks

--- videos-hooks/src/App.js
+++ videos-hooks/src/App.js
@@ -1,17 +1,20 @@
 import React, {useState, useEffect} from "react";
 import SearchBar from "./components/SearchBar";
-import youtube from "./apis/youtube";
 import VideoList from "./components/VideoList";
 import VideoDetail from "./components/VideoDetail";
+import useVideos from "./hooks/useVideos";
 
 const App = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos('cake');
 
-  setSelectedVideo(response.data.items[0]);
+  useEffect(() => {
+      setSelectedVideo(videos[0]);
+  }, [videos]);
 
   return ( 
     <div className="ui container">
-      <SearchBar onFormSubmit={onTermSubmit} />
+      <SearchBar onFormSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
