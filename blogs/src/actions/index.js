341801b18dd14b5cf4429887de6e2c841daf98e5
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2022-12-12 16:25:38 +0000

    fix: use redux

--- blogs/src/actions/index.js
+++ blogs/src/actions/index.js
@@ -1,10 +1,12 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPosts = async () => {
-  const response = await jsonPlaceholder.get('/posts');
+  return function(dispatch, getState) {
+    const response = await jsonPlaceholder.get('/posts');
 
-  return {
-    type: 'FETCH_POSTS',
-    payload: response
+    return {
+      type: 'FETCH_POSTS',
+      payload: response
+    };
   };
 };
