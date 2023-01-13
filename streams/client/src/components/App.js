ef3a95339a8acb5ad50ba613608854d724f821af
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2023-01-13 16:44:13 +0000

    fix: testing out react router

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,5 +1,28 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const PageOne = () => {
+  return <div>Page One</div>
+};
+
+const PageTwo = () => {
+  return (
+  <div>
+    Page Two
+    <button>Click me</button>
+  </div>)
+};
+
 const App = () => {
-  return ( <div>App</div> );
+  return ( 
+    <div>
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={PageOne} />
+          <Route path="/pagetwo" component={PageTwo} />
+        </div>
+      </BrowserRouter>
+    </div> 
+  );
 }
  
 export default App;
