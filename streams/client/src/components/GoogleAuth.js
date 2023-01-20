1a68be2e2fcd33bbb1d1ea5745b26d17bad820b4
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2023-01-20 15:59:06 +0000

    fix: add sign in check

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -11,10 +11,15 @@ class GoogleAuth extends React.Component {
         plugin_name: "streamy",
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
   }
+
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
 
   renderAuthButton() {
     if ( this.state.isSignedIn === null ) {
