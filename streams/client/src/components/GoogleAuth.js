26a4781b6b089319eb7ed8d95149061ee600d4f3
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2023-01-20 08:44:57 +0000

    chore: update clientId and add plugin name

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -4,8 +4,9 @@ class GoogleAuth extends React.Component {
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
-        clientId: '650648567623-hrc5r8c84g0ob6eerohd0l8dul3a60aq.apps.googleusercontent.com',
-        scope: 'email'
+        clientId: '797401886567-9cumct9mrt3v2va409rasa7fa6fq02hh.apps.googleusercontent.com',
+        scope: 'email',
+        plugin_name: "streamy",
       });
     });
   }
