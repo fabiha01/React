6dc8bf9d9ec2d1b4c6d991c877499c5bbe535b50
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2023-01-23 08:51:28 +0000

    fix: import reducers and actions

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>,   
   </React.StrictMode>
 );
