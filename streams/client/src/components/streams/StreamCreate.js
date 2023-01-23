1fa36cca7efc1fab47747d36012723eca0e033f1
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2023-01-23 14:33:37 +0000

    fix: streamCreate form created using reduxForm

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,9 +1,20 @@
-const StreamCreate = () => {
-  return ( 
-    <div>
-      StreamCreate
-    </div> 
-  );
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+class StreamCreate extends React.Component {
+  renderInput({ input }) {
+    return <input {...input} />
+  }
+
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
+      </form>
+    );
+  }
 }
- 
-export default StreamCreate;
+
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
