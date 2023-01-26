2f631d7d3602cdb9ad60d1af940fdd28e03453ad
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2023-01-26 12:37:44 +0000

    fix: use class based component to fetch streams

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,9 +1,17 @@
-const StreamList = () => {
-  return ( 
-    <div>
-      StreamList
-    </div> 
-  );
-}
- 
-export default StreamList;
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
+
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+
+  render() {
+    return (
+      <div>StreamList</div>
+    )
+  }
+};
+
+export default connect(null, { fetchStreams })(StreamList);
