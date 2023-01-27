882a2ca7426c53c6c7586b466369b232f1fdbb4c
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2023-01-27 08:48:01 +0000

    fix: add edit/delete buttons

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { fetchStreams } from '../../actions';
+import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
 
 class StreamList extends React.Component {
   componentDidMount() {
@@ -9,7 +9,12 @@ class StreamList extends React.Component {
 
   renderAdmin(stream) {
     if (stream.userId === this.props.currentUserId) {
-      return <div>EDIT/DELETE</div>
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
     }
   }
 
@@ -17,12 +22,12 @@ class StreamList extends React.Component {
     return this.props.streams.map( stream => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera"></i>
           <div className="content">
             {stream.title}
             <div className="description">{stream.description}</div>
           </div>
-          {this.renderAdmin(stream)}
         </div>
       );
     });
@@ -34,14 +39,14 @@ class StreamList extends React.Component {
         <h2>Streams</h2>
         <div className="ui celled list">{this.renderList()}</div>
       </div>
-    )
+    );
   }
-};
+}
 
-const mapStateToProps = state => {
-  return { 
+const mapStateToProps = (state) => {
+  return {
     streams: Object.values(state.streams),
-    currentUserId: state.auth.userId
+    currentUserId: state.auth.userId,
   };
 };
 
