5ae669dd72ca306afc94368e3f4d1b3104e83770
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2023-01-30 16:05:04 +0000

    fix: pass props to modal

--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,12 +1,23 @@
 import Modal from "../Modal";
 
 const StreamDelete = () => {
-  return ( 
+  const actions = (
+    <div>
+      <button className="ui button negative">Delete</button>
+      <button className="ui button">Cancel</button>
+    </div>
+  );
+
+  return (
     <div>
       StreamDelete
-      <Modal />
-    </div> 
+      <Modal
+        title="Delete Stream"
+        content="Are you sure you want to delete this stream?"
+        actions={actions}
+      />
+    </div>
   );
-}
- 
+};
+
 export default StreamDelete;
