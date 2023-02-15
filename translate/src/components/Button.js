77a9581a09aaf9657edf5de9166ccffd53cb7b18
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2023-02-15 09:06:25 +0000

    chore: remove console log

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -5,7 +5,6 @@ class Button extends React.Component {
   static contextType = LanguageContexts;
 
   render() {
-    console.log(this.context);
     return <button className="ui button primary">Submit</button>
   }
 }
