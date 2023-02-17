023d05a5cce43d1e01cfa48136e773f0b836ad1f
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2023-02-17 08:59:14 +0000

    fix: use the language store

--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -3,15 +3,15 @@ import LanguageContexts from "../contexts/LanguageContexts";
 import ColourContext from "../contexts/ColourContext";
 
 class Button extends React.Component {
-  renderSubmit(value) {
-    return value === "english" ? "Submit" : "Voorleggen";
+  renderSubmit(language) {
+    return language === "english" ? "Submit" : "Voorleggen";
   }
 
   renderButton(color) {
     return (
       <button className={`ui button ${color}`}>
         <LanguageContexts.Consumer>
-          {(value) => this.renderSubmit(value)}
+          {({language}) => this.renderSubmit(language)}
         </LanguageContexts.Consumer>
       </button>
     );
