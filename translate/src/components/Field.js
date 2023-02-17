9f26ccc81693546503310a62587eedf32e3e1911
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2023-02-17 08:59:24 +0000

    fix: use the language store

--- translate/src/components/Field.js
+++ translate/src/components/Field.js
@@ -5,7 +5,7 @@ class Field extends React.Component {
   static contextType = LanguageContexts;
 
   render() {
-    const text = this.context === 'english' ? 'Name' : 'Naam';
+    const text = this.context.language === 'english' ? 'Name' : 'Naam';
 
     return (
       <div className="ui field">
