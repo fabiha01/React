de0d513ef2bdc572f8f36eea02cf0d70e3fbdef4
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2023-02-17 08:48:36 +0000

    fix: import language store

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,25 +1,19 @@
 import React from "react";
-import UserCreate from '../components/userCreate';
-import LanguageContexts from "../contexts/LanguageContexts";
+import UserCreate from "../components/userCreate";
+import { LanguageStore } from "../contexts/LanguageContexts";
 import ColourContext from "../contexts/ColourContext";
 import LanguageSelector from "./LanguageSelector";
 
 class App extends React.Component {
-  state = { language: "english" };
-
-  onLanguageChange = language => {
-    this.setState({ language });
-  };
-
   render() {
     return (
       <div className="ui container">
-        <LanguageSelector onLanguageChange={this.onLanguageChange} />
-        <LanguageContexts.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
           <ColourContext.Provider value="red">
             <UserCreate />
           </ColourContext.Provider>
-        </LanguageContexts.Provider>
+        </LanguageStore>
       </div>
     );
   }
