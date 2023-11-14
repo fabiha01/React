5d86ca2d2d5bfd5d58a757b80b57ac229d5f8f4a
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2023-11-14 16:07:18 +0000

    fix: learn how to use useState

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,13 +1,20 @@
+import { useState } from "react";
+
 const Home = () => {
 
-  const handleClick = ( name, e ) => {
-    console.log(`hello ${name}`, e);
+  const [name, setName] = useState('mario');
+  const [age, setAge] = useState(25);
+
+  const handleClick = ( ) => {
+    setName('luigi');
+    setAge(35);
   }
 
   return ( 
     <div className="home">
       <h2>Homepage</h2>
-      <button onClick={(e) => handleClick('yoshi', e)}>Click Me</button>
+      <p>{name} is {age} years old</p>
+      <button onClick={handleClick}>Click Me</button>
     </div>
    );
 }
