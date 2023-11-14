c1adf868218208de0a1f4c0fb60d3a59bdf3a661
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2023-11-14 15:59:59 +0000

    fix: react to click events

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+
+  const handleClick = ( name, e ) => {
+    console.log(`hello ${name}`, e);
+  }
+
   return ( 
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={(e) => handleClick('yoshi', e)}>Click Me</button>
     </div>
    );
 }
