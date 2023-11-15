9ae3b9343c39f8870b1decfb3c952f40c37ffbff
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2023-11-15 15:00:23 +0000

    fix: create handleDelete function and pass as props

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,19 +2,22 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-
   const [blogs, setBlogs] = useState([
     { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
     { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ]);
+  ])
 
-  return ( 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+  }
+
+  return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!"/>
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario' )} title="Mario's Blogs"/>
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
     </div>
-   );
+  );
 }
  
 export default Home;
