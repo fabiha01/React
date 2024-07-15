17745c43943608e555c235a4fdd9b318c9cff30a
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2024-07-15 10:52:28 +0100

    chore: remove import

--- new-blog/src/App.js
+++ new-blog/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <p>This is a new paragraph</p>
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
