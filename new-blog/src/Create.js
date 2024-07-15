2c60477326c93b1039b33289c8e1aaa569be5720
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2024-07-15 17:06:12 +0100

    fix: add state for the input fields

--- new-blog/src/Create.js
+++ new-blog/src/Create.js
@@ -1,24 +1,38 @@
+import { useState } from "react";
+
 const Create = () => {
-  return ( 
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+  return (
     <div className="create">
       <h2>Add a new blog</h2>
       <form>
         <label>Blog Title</label>
-        <input 
+        <input
           type="text"
           required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
-      <label>Blog Body</label>
-      <textarea required></textarea>
-      <label>Blog Author</label>
-      <select>
-        <option value="mario">Mario</option>
-        <option value="yoshi">Yoshi</option>
-      </select>
-      <button>Add Blog</button>
+        <label>Blog Body</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog Author</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">Mario</option>
+          <option value="yoshi">Yoshi</option>
+        </select>
+        <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
+        <p>{author}</p>
       </form>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Create;
